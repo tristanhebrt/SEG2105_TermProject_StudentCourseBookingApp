b8612e902a53c196e4c2b4c88cd59f984341e8c3
Tristan Hebert <91288854+tristanhebrt@users.noreply.github.com>
2022-10-19 23:48:02 -0400

    Declared TextView and Buttons, added a very basic admin login functionality with display if login was successful or not.

--- app/src/main/java/com/example/studentcoursebookingapp/MainActivity.java
+++ app/src/main/java/com/example/studentcoursebookingapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.studentcoursebookingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
 
+        Button createAccBtn = (Button) findViewById(R.id.createAccount);
+        Button loginBtn = (Button) findViewById(R.id.login);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin123")){
+                    //correct
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                }else{
+                    //incorrect
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
