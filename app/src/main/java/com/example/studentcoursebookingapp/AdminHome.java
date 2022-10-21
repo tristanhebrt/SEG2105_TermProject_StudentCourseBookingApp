bc99533d96348d2f16100b23aa8c0fa9ab1449d0
Tristan Hebert <91288854+tristanhebrt@users.noreply.github.com>
2022-10-21 15:23:58 -0400

    Changed layout to be relative. Fixed the method used to change activity. Added on click listeners to the AdminHome buttons. Cleaned up the code.

--- app/src/main/java/com/example/studentcoursebookingapp/AdminHome.java
+++ app/src/main/java/com/example/studentcoursebookingapp/AdminHome.java
@@ -2,13 +2,54 @@ package com.example.studentcoursebookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class AdminHome extends AppCompatActivity {
+import java.util.Arrays;
+
+public class AdminHome extends AppCompatActivity implements View.OnClickListener {
+    Button createCourse, editCourse, deleteCourse, deleteAccounts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+
+        createCourse = (Button) findViewById(R.id.createCourse);
+        editCourse = (Button) findViewById(R.id.editCourse);
+        deleteCourse = (Button) findViewById(R.id.deleteCourse);
+        deleteAccounts = (Button) findViewById(R.id.deleteAccounts);
+
+        setClickListeners();
+    }
+
+    private void setClickListeners() {
+        for (Button button : Arrays.asList(createCourse, editCourse, deleteCourse,
+                deleteAccounts)) {
+            button.setOnClickListener(this);
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.createCourse:
+                Toast.makeText(AdminHome.this, "CREATE COURSE SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.editCourse:
+                Toast.makeText(AdminHome.this, "EDIT COURSE SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.deleteCourse:
+                Toast.makeText(AdminHome.this, "DELETE COURSE SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.deleteAccounts:
+                Toast.makeText(AdminHome.this, "DELETE ACCOUNT SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/studentcoursebookingapp/MainActivity.java
+++ app/src/main/java/com/example/studentcoursebookingapp/MainActivity.java
@@ -2,37 +2,62 @@ package com.example.studentcoursebookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView username, password;
+    Button createAccount, login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
+        username = (TextView) findViewById(R.id.username);
+        password = (TextView) findViewById(R.id.password);
 
-        Button createAccount = (Button) findViewById(R.id.createAccount);
-        Button login = (Button) findViewById(R.id.login);
+        createAccount = (Button) findViewById(R.id.createAccount);
+        login = (Button) findViewById(R.id.login);
 
-        login.setOnClickListener(new View.OnClickListener() {                                                              // when the <Button login> is clicked
-            @Override
-            public void onClick(View view) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin123")){     // if the <TextView username> is admin <TextView password> is admin123
+        setClickListeners();
+    }
+
+    private void setClickListeners() {
+        for (Button button : Arrays.asList(createAccount, login)) {
+            button.setOnClickListener(this);
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.createAccount:
+                Toast.makeText(this, "CREATE ACCOUNT", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.login:
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin123")) {     // if the <TextView username> is admin <TextView password> is admin123
                     //correct
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();     // notify "LOGIN SUCCESSFUL"
-                    setContentView(R.layout.activity_admin_home);                                                          // bring to AdminHome screen
-                }else{                                                                                                     // if not
+                    openAdminHome();
+                } else {                                                                                                     // if not
                     //incorrect
                     Toast.makeText(MainActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();         // notify "LOGIN FAILED"
                 }
-            }
-        });
+                break;
+        }
+    }
+
+    public void openAdminHome() {
+        Intent intent = new Intent(this, AdminHome.class);
+        startActivity(intent);
     }
 }
